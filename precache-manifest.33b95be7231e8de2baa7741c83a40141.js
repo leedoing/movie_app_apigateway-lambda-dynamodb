09534d81570982fb73a8d5b77856cda1e2762647
leedoing <lluckyy77@gmail.com>
2018-12-20 00:20:19 +0900

    Updates

---
+++ precache-manifest.33b95be7231e8de2baa7741c83a40141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2665d2e3ef69e35da6",
+    "url": "/movie_app_apigateway-lambda-dynamodb/static/js/runtime~main.0d2665d2.js"
+  },
+  {
+    "revision": "f9bdda14d6f02cf623be",
+    "url": "/movie_app_apigateway-lambda-dynamodb/static/js/main.f9bdda14.chunk.js"
+  },
+  {
+    "revision": "d0c28ac72245ff7380f5",
+    "url": "/movie_app_apigateway-lambda-dynamodb/static/js/1.d0c28ac7.chunk.js"
+  },
+  {
+    "revision": "f9bdda14d6f02cf623be",
+    "url": "/movie_app_apigateway-lambda-dynamodb/static/css/main.ac3b086f.chunk.css"
+  },
+  {
+    "revision": "7025e2720998f5580470fcf1fbd92f98",
+    "url": "/movie_app_apigateway-lambda-dynamodb/index.html"
+  }
+];
